d79896ff00f5772d2d7fac33ceec2f0b70136a7c
Narasimha <satyansv5@gmail.com>
2023-12-05 18:35:53 +0530

    https://github.com/Narasimha2112/Python

---
+++ posneg.py
@@ -0,0 +1,10 @@
+"""Write a program to check whether the given number is positive or negative ??? """
+
+x=eval(input("enter a number: "))
+
+if(x>0):
+    print("Positive number")
+elif(x<0):
+    print("Negative number")
+else:
+    print("Zero")
