a42623c79b51a7386d4ca3eecd504bf7b765c0b0
Narasimha <satyansv5@gmail.com>
2023-12-05 18:18:04 +0530

    first commit

---
+++ evenodd.py
@@ -0,0 +1,9 @@
+"""Write a program to check whether the given number is even or odd???"""
+
+x=eval(input("enter a number: "))
+
+if(x%2==0):
+    print("even number")
+else:
+    print("odd number")
+
